7a8d4dfee51561f60814684309624670f0fd67b8
Jatin Dalla <78598294+jatin452@users.noreply.github.com>
2024-03-19 17:13:20 +0530

    Update Sample.ps1

--- Sample.ps1
+++ Sample.ps1
@@ -3,8 +3,9 @@ Write-Host "This script is running as part of the build process."
 Write-Host "You can add any commands or logic here that you need for your build."
 
 # Redirect script output to a file
-$outputFile = "script_output.txt"
+$outputFile = "$(Build.ArtifactStagingDirectory)\script_output.txt"
 "Output from the PowerShell script." | Out-File -FilePath $outputFile
 
 # Publish the output file as a build artifact
-Write-Host "##vso[task.uploadfile]$outputFile"
+Write-Host "Publishing output file '$outputFile' as a build artifact."
+Write-Output "##vso[task.uploadfile]$outputFile"
