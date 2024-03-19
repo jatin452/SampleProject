17cec080966b9f50a99a0d5742913e8f045cea88
Jatin Dalla <78598294+jatin452@users.noreply.github.com>
2024-03-19 17:03:51 +0530

    Add files via upload

--- Sample.ps1
+++ Sample.ps1
@@ -4,3 +4,10 @@
 echo "Hello from the shell script!"
 echo "This script is running as part of the build process."
 echo "You can add any commands or logic here that you need for your build."
+
+# Redirect script output to a file
+output_file="script_output.txt"
+echo "Output from the shell script." > "$output_file"
+
+# Publish the output file as a build artifact
+echo "##vso[task.uploadfile]$output_file"
