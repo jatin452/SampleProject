d0e2a5b4be34c515bd227895cb9b9bb4efd53176
Jatin Dalla <78598294+jatin452@users.noreply.github.com>
2024-03-19 18:11:00 +0530

    Update Sample.ps1

--- Sample.ps1
+++ Sample.ps1
@@ -6,6 +6,11 @@ Write-Host "Hello from the PowerShell script!"
 Write-Host "This script is running as part of the build process."
 Write-Host "You can add any commands or logic here that you need for your build."
 
+# Create the output directory if it does not exist
+if (-not (Test-Path -Path $outputDirectory)) {
+    New-Item -ItemType Directory -Path $outputDirectory | Out-Null
+}
+
 # Define the output file location within the provided directory
 $outputFile = "$outputDirectory\script_output.txt"
 "Output from the PowerShell script." | Out-File -FilePath $outputFile
