e8b281654bc502b5198f4dcc232759938e979557
Jatin Dalla <78598294+jatin452@users.noreply.github.com>
2024-03-19 17:07:26 +0530

    Add files via upload

--- Sample.ps1
+++ Sample.ps1
@@ -1,13 +1,10 @@
-#!/bin/bash
-
-# This is a sample shell script
-echo "Hello from the shell script!"
-echo "This script is running as part of the build process."
-echo "You can add any commands or logic here that you need for your build."
+Write-Host "Hello from the PowerShell script!"
+Write-Host "This script is running as part of the build process."
+Write-Host "You can add any commands or logic here that you need for your build."
 
 # Redirect script output to a file
-output_file="script_output.txt"
-echo "Output from the shell script." > "$output_file"
+$outputFile = "script_output.txt"
+"Output from the PowerShell script." | Out-File -FilePath $outputFile
 
 # Publish the output file as a build artifact
-echo "##vso[task.uploadfile]$output_file"
+Write-Host "##vso[task.uploadfile]$outputFile"
