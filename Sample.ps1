4c6d7207c8a3b2a5547585fbeace2608d4a70e9a
Jatin Dalla <78598294+jatin452@users.noreply.github.com>
2024-03-19 17:35:10 +0530

    Update Sample.ps1

--- Sample.ps1
+++ Sample.ps1
@@ -1,9 +1,13 @@
+param (
+    [string]$outputDirectory
+)
+
 Write-Host "Hello from the PowerShell script!"
 Write-Host "This script is running as part of the build process."
 Write-Host "You can add any commands or logic here that you need for your build."
 
-# Define the output file location within the GitHub repository
-$outputFile = "$(System.DefaultWorkingDirectory)\script_output.txt"
+# Define the output file location within the provided directory
+$outputFile = "$outputDirectory\script_output.txt"
 "Output from the PowerShell script." | Out-File -FilePath $outputFile
 
 # Publish the output file as a build artifact (if needed)
